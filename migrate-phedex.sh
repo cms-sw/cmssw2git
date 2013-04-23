b6ae446ef6fb44f4cfb2a2e9acd5d10149cdf4c8
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-04-23 08:23:42 +0200

    Do not hardcode email substitutions.

--- migrate-phedex.sh
+++ migrate-phedex.sh
@@ -23,12 +23,6 @@ time rsync -av --delete --delete-excluded \
 time /build/ge/test-git/sw/usr/bin/cvs2git --options cvs2git-$PROJECT_NAME.options --pass 1:16
 rm -rf $PROJECT_NAME.git
 git init --bare $PROJECT_NAME.git
-perl -p -i -e 's/^committer cmsbuild <>/committer Giulio Eulisse <sha1-a598d8593b5db48d5829590a227bbe04f25460b4\@cern.ch>/; \
-              s/^committer muzaffar <>/committer Shahzad Malik Muzaffar <sha1-dc0e3810c4d4d85d6295265f1fb210880ec3f132\@cern.ch>/; \
-              s/^committer elmer <>/committer Peter Elmer <sha1-80460c2ac58e1aa0fd289ec2aa9d168a17bd6eb2\@cern.ch>/; \
-              s/^committer sashby <>/committer Shaun Ashby <sha1-0665f252eb8e02afcb80198b20033a58c31fd1b3\@cern.ch>/; \
-              s/^committer williamc <>/committer Christopher Williams <sha1-ebcb402bd5472f352dd1d1c7a94fa0cbb49cf4a3\@cern.ch>/;' \
-              cvs2git-$PROJECT_NAME-tmp/git-dump.dat
 pushd $PROJECT_NAME.git
   cat ../cvs2git-$PROJECT_NAME-tmp/git-blob.dat ../cvs2git-$PROJECT_NAME-tmp/git-dump.dat | git fast-import
   git gc --prune=now --aggressive
